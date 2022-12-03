bc8f2104aeeadad0107736effda501626c7d9df3
AbooMinister25 <aboominister@gmail.com>
2022-12-03 17:52:53 -0500

    Refactored the solution for part-1 a bit

--- day3/day3_python/src/main.py
+++ day3/day3_python/src/main.py
@@ -2,18 +2,17 @@ import string
 import functools
 
 from aoc_lube import fetch, submit
-from more_itertools import chunked
+from more_itertools import chunked, divide
 
 
 priorities = dict(zip(string.ascii_letters, range(1, 53)))
 
 
 def part_1(data: list[str]) -> int:
-    halves = [
-        next(iter(set(i[: int(len(i) / 2)]) & set(i[int(len(i) / 2) :]))) for i in data
-    ]
-    priorities_sum = sum(priorities[s] for s in halves)
-
+    priorities_sum = sum(
+        [priorities[(set(a) & set(b)).pop()] for a, b in [divide(2, i) for i in data]]
+    )
+    
     return priorities_sum
 
 
@@ -29,5 +28,5 @@ if __name__ == "__main__":
     data = fetch(2022, 3).splitlines()
     part_1(data)
 
-    submit(2022, 3, 1, functools.partial(part_1, data))
-    submit(2022, 3, 2, functools.partial(part_2, data))
+    print(part_1(data))
+    print(part_2(data))
